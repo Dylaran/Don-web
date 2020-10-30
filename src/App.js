952dda04dd24f00ecf7d0dbf3029858779911e3a
Don Nakashima <nakashi6.msu.edu>
2020-10-30 15:33:40 -0400

    Added pic, editted page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 //import logo from './logo.svg';
+import don from './don.jpg';
 import './App.css';
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <img src="don.jpg" alt="don" />
+        <img src={don} alt="don" />
         <p>
             {/*Edit <code>src/App.js</code> and save to reload.*/}
             Don Nakashima
