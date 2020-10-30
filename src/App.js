8a941fc10869b50f6d73b4aaf4e3dbef1dace783
Don Nakashima <nakashi6.msu.edu>
2020-10-30 10:33:58 -0400

    Change to app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br />
+          This is Don's website.
+          
         </p>
         <a
           className="App-link"
