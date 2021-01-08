aa47f5efd8cf142e5e258fcaab709920a4549cd7
Don Nakashima <nakashi6.msu.edu>
2021-01-08 04:29:55 -0500

    Added more projects to portfolio

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -55,6 +55,76 @@ class Portfolio extends Component {
                         </a>
                     </div>
                 </div>
+                <div className="columns portfolio-item">
+                    <div className="item-wrap">
+                        <a href="https://github.com/mdlandis/game-jam-project" title="Seconds">
+                        <img alt="Seconds" src="images/portfolio/seconds.png" />
+                        <div className="overlay">
+                            <div className="portfolio-item-meta">
+                            <h5>Seconds</h5>
+                                <p>A speed run game developed during Make School game jam</p>
+                            </div>
+                            </div>
+                        <div className="link-icon"><i className="fa fa-link"></i></div>
+                        </a>
+                    </div>
+                </div>
+                <div className="columns portfolio-item">
+                    <div className="item-wrap">
+                        <a href="https://github.com/Dylaran/NoSQL" title="NoSQL">
+                        <img alt="NoSQL" src="images/portfolio/github.png" />
+                        <div className="overlay">
+                            <div className="portfolio-item-meta">
+                            <h5>NoSQL</h5>
+                                <p>Implementation of a Document Store, a type of database with no schema</p>
+                            </div>
+                            </div>
+                        <div className="link-icon"><i className="fa fa-link"></i></div>
+                        </a>
+                    </div>
+                </div>
+                <div className="columns portfolio-item">
+                    <div className="item-wrap">
+                        <a href="https://github.com/Dylaran/Sqlite3Emulator" title="Sqlite3Emulator">
+                        <img alt="Sqlite3Emulator" src="images/portfolio/github.png" />
+                        <div className="overlay">
+                            <div className="portfolio-item-meta">
+                            <h5>Sqlite 3 Emulator</h5>
+                                <p>Python module that emulates the behaviour of the built-in module sqlite3 that executes SQL statements</p>
+                            </div>
+                            </div>
+                        <div className="link-icon"><i className="fa fa-link"></i></div>
+                        </a>
+                    </div>
+                </div>
+                <div className="columns portfolio-item">
+                    <div className="item-wrap">
+                        <a href="https://github.com/Dylaran/FileFormatConversions" title="FileFormatConversions">
+                        <img alt="FileFormatConversions" src="images/portfolio/github.png" />
+                        <div className="overlay">
+                            <div className="portfolio-item-meta">
+                            <h5>File Format Conversions</h5>
+                                <p>Converts between 3 plain text data representations: CSV, JSON, and XML</p>
+                            </div>
+                            </div>
+                        <div className="link-icon"><i className="fa fa-link"></i></div>
+                        </a>
+                    </div>
+                </div>
+                <div className="columns portfolio-item">
+                    <div className="item-wrap">
+                        <a href="https://github.com/Dylaran/UnityRayTracing" title="UnityRayTracing">
+                        <img alt="UnityRayTracing" src="images/portfolio/github.png" />
+                        <div className="overlay">
+                            <div className="portfolio-item-meta">
+                            <h5>Unity Ray Tracing</h5>
+                                <p>CSE term project to explore ray tracing in Unity for a computer graphics course</p>
+                            </div>
+                        </div>
+                        <div className="link-icon"><i className="fa fa-link"></i></div>
+                        </a>
+                    </div>
+                </div>
             </div>
           </div>
       </div>
