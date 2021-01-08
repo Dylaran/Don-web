eaeddae336566c4f12d3f28814b0df2eb622d5fb
Don Nakashima <nakashi6.msu.edu>
2021-01-08 04:04:10 -0500

    Added QOL features

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,24 +2,23 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
-
-    if(this.props.data){
-      var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
-      })
-    }
-
     return (
       <footer>
 
      <div className="row">
         <div className="twelve columns">
-           <ul className="social-links">
-              {networks}
-           </ul>
+            <ul className="social">
+                <li><a href={"https://www.facebook.com/DonNakashima/"}><i className={"fa fa-facebook"}></i></a></li>
+                <li><a href={"https://www.twitter.com/DonNakashima"}><i className={"fa fa-twitter"}></i></a></li>
+                <li><a href={"https://www.instagram.com/DonNakashima/"}><i className={"fa fa-instagram"}></i></a></li>
+                <li><a href={"https://twitch.tv/riceballs"}><i className={"fa fa-twitch"}></i></a></li>
+                <li><a href={"https://www.linkedin.com/in/donnaka/"}><i className={"fa fa-linkedin"}></i></a></li>
+                <li><a href={"https://www.github.com/dylaran"}><i className={"fa fa-github"}></i></a></li>
+                <li><a href={"https://riceballs.itch.io"}><i className="fab fa-itch-io"></i></a></li>
+            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2020 Don Nakashima</li>
+              <li>&copy; 2020 @ Don Nakashima</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
